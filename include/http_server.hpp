ae63c3af113586f409846084f8947405708137ad
zlovtnik <racasantos@icloud.com>
2025-08-11 11:35:48 -0400

    feat(http_server): Implement connection pooling and timeout management

--- include/http_server.hpp
+++ include/http_server.hpp
@@ -20,10 +20,14 @@ class RequestHandler;
 class WebSocketManager;
 class ETLJobManager;
 class JobMonitorService;
+class ConnectionPoolManager;
+class TimeoutManager;
+struct ServerConfig;
 
 class HttpServer {
 public:
   HttpServer(const std::string &address, unsigned short port, int threads = 1);
+  HttpServer(const std::string &address, unsigned short port, int threads, const ServerConfig& config);
   ~HttpServer();
 
   void start();
@@ -32,6 +36,14 @@ public:
 
   void setRequestHandler(std::shared_ptr<RequestHandler> handler);
   void setWebSocketManager(std::shared_ptr<WebSocketManager> wsManager);
+
+  // Configuration management
+  void setServerConfig(const ServerConfig& config);
+  ServerConfig getServerConfig() const;
+
+  // Connection pool management
+  std::shared_ptr<ConnectionPoolManager> getConnectionPoolManager();
+  std::shared_ptr<TimeoutManager> getTimeoutManager();
 
   // Add getters for testing purposes
   std::shared_ptr<ETLJobManager> getJobManager();
